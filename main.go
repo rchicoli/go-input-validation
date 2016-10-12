50ae90d4386db05e7703542af4a6799bf5557f85
Rafael Chicoli <rafaelchicoli@hotmail.com>
2016-10-12 23:45:24 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+)
+
+var emailValidation = regexp.MustCompile(`\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z`)
+
+type Person struct {
+	Email string `json:"email"`
+}
+
+func Validate(p Person) error {
+	if !emailValidation.MatchString(p.Email) {
+		return fmt.Errorf("NOT VALID")
+	}
+	return nil
+}
+
+type InputValidation interface {
+	Validate(r *http.Request) error
+}
+
+var f http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+	var person *Person
+	err := decodeAndValidate(r, person)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("%v", err)))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func decodeAndValidate(r *http.Request, p *Person) error {
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	return Validate(*p)
+}
+
+func main() {
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: f,
+	}
+	srv.ListenAndServe()
+}
